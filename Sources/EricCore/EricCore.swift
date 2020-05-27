47f76898bf7dee5c4df4b49fb89179c1c5c3df8e
Hsinyichen <eric2asd@gmail.com>
2020-05-27 16:17:46 +0800

    Providing static variable for a color pallette

--- Sources/EricCore/EricCore.swift
+++ Sources/EricCore/EricCore.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class EricCore {
+public class EricCore {
   
   /// Allows you to convert a 6 digit hexadecimal string into a UIColor instance
   /// - Warming: The "#" symbol is stripped from the beginning of the string submitted here.
@@ -8,7 +8,7 @@ class EricCore {
   ///   - hexString: A 6-digit hexadecimal string. Use 6 digits rather than 8, and add the accompanying alpha value in the second parameter
   ///   - alpha: A number between 0.0 and 1.0 indicating how transparent the color is
   /// - Returns: A UIColor defined by the `hexString` parameter
-  class func colorFromHexString(_ hexString: String, alpha: CGFloat = 1.0) -> UIColor {
+  internal class func colorFromHexString(_ hexString: String, alpha: CGFloat = 1.0) -> UIColor {
     let r, g, b: CGFloat
     let offset = hexString.hasPrefix("#") ? 1 : 0
     let start = hexString.index(hexString.startIndex, offsetBy: offset)
@@ -22,5 +22,10 @@ class EricCore {
       return UIColor(red: r, green: g, blue: b, alpha: alpha)
     }
     return UIColor(red: 0, green: 0, blue: 0, alpha: alpha)
+  }
+  
+  /// The most eye-pleasing color known to all humanity
+  public static var ericColor: UIColor {
+    return self.colorFromHexString("006736")
   }
 }
--- Tests/EricCoreTests/EricCoreTests.swift
+++ Tests/EricCoreTests/EricCoreTests.swift
@@ -7,6 +7,11 @@ final class EricCoreTests: XCTestCase {
     let color = EricCore.colorFromHexString("FF0000")
     XCTAssertEqual(color, .red)
   }
+  
+  func testEricColorsAreEqual() {
+    let color = EricCore.colorFromHexString("006736")
+    XCTAssertEqual(color, EricCore.ericColor)
+  }
 
     static var allTests = [
         ("testColorRedEqual", testColorRedEqual),
